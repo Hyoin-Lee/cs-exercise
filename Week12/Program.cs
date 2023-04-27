c162a10dad813e65e1ffb7727625dfecb1a833fb
Scott <131235485+Hyoin-Lee@users.noreply.github.com>
2023-04-27 16:17:10 +0800

    Add files via upload

---
+++ Week12/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Week12;
+
+Shape shape = new Circle(5);
+//Console.WriteLine(Shape.GetArea());
+
+Shape[] Shape =
+{
+    shape,
+    new Rectangle(8,7),
+    new Square(5),
+    new Circle(10),
+new Rectangle(10, 2)
+};
+
+for (int i = 0; i < Shape.Length; i++)
+{
+    Console.WriteLine(Shape[i].GetArea());
+
+
+}
---
+++ Week12/Quadrailateral.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Week12
+{
+    internal abstract class Quadrailateral : Shape
+    {
+
+
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Quadrailateral()
+        {
+            name = "Quadrailateral";
+
+
+        }
+
+        public override double GetArea()
+        {
+            Console.WriteLine($"[{name}] Get Area:");
+            return Length * Width;
+        }
+    }
+}
